5ba874fa1f3451800aa15e0d2f126a253ea34a42
Dheeraj Singh <your.email@dheerajsingh2100@gmail.com>
2024-12-15 14:50:18 +0530

    setup redux store

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,79 @@
-import React from "react";
-
+import React, { useState } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { BASE_URL } from "../utils/constants";
+import { useNavigate } from "react-router-dom";
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("dheeraj@gmail.com");
+
+  const [password, setPassword] = useState("Dheeraj@123");
+
+  const dispatch = useDispatch();
+
+  const navigate=useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/login",
+        {
+          emailId: email,
+          password,
+        },
+        {
+          withCredentials: true,
+        }
+      );
+      dispatch(addUser(res.data));
+      return navigate('/');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className="flex justify-center my-[6rem]">
+      <div className="card bg-base-300 w-96 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <div>
+            <label className="form-control w-full max-w-xs my-2">
+              <div className="label">
+                <span className="label-text">Email Id</span>
+              </div>
+              <input
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+                type="email"
+                className="input input-bordered w-full max-w-xs"
+              />
+            </label>
+            <label className="form-control w-full max-w-xs my-2">
+              <div className="label">
+                <span className="label-text">Password</span>
+              </div>
+              <input
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                type="password"
+                className="input input-bordered w-full max-w-xs"
+              />
+            </label>
+          </div>
+          <div className="card-actions justify-center m-2">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
---
+++ src/utils/appStore.js
@@ -0,0 +1,12 @@
+import reducer from "./userSlice";
+import userReducer from "./userSlice";
+
+import { configureStore } from "@reduxjs/toolkit";
+
+const appStore = configureStore({
+  reducer: {
+    user: userReducer,
+  },
+});
+
+export default appStore;
