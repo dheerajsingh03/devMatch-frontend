3c2d5d0edf65649d8d5fcdfbcb6dd9361df6b254
Dheeraj Singh <your.email@dheerajsingh2100@gmail.com>
2025-04-22 16:53:04 +0530

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,33 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Provider } from "react-redux";
-import { useEffect } from "react";
-import appStore from "./utils/appStore";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Body from "./components/Body";
 import Login from "./components/Login";
-import Feed from "./components/Feed";
 import Profile from "./components/Profile";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
+import Feed from "./components/Feed";
 import Connections from "./components/Connections";
-import Request from "./components/Request";
-import PrivacyPolicy from "./components/PrivacyPolicy";
-import TermsAndConditions from "./components/TermsAndConditions";
-import RefundPolicy from "./components/RefundPolicy";
-import ShippingPolicy from "./components/ShippingPolicy";
-import ContactUs from "./components/ContactUs";
+import Requests from "./components/Requests";
 import Premium from "./components/Premium";
 
 function App() {
-  useEffect(() => {
-    document.title = "DevMatch";
-  }, []);
-
   return (
-    <Provider store={appStore}>
-      <BrowserRouter basename="/">
-        <Routes>
-          {/* Public Routes */}
-          <Route path="/login" element={<Login />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-          <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
-          <Route path="/refund-policy" element={<RefundPolicy />} />
-          <Route path="/shipping-policy" element={<ShippingPolicy />} />
-          <Route path="/contact-us" element={<ContactUs />} />
-
-          {/* Protected Routes */}
-          <Route path="/" element={<Body />}>
-            <Route index element={<Feed />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/connections" element={<Connections />} />
-            <Route path="/requests" element={<Request />} />
-            <Route path="/premium" element={<Premium />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/" element={<Feed />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/connections" element={<Connections />} />
+              <Route path="/requests" element={<Requests />} />
+              <Route path="/premium" element={<Premium />} />
+              
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </>
   );
 }
 
