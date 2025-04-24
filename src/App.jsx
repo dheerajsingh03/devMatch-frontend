ae5a7d8fb85de03439589c9778006a5f2b73357f
Dheeraj Singh <your.email@dheerajsingh2100@gmail.com>
2025-04-24 09:59:12 +0530

    added chat feature

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Feed from "./components/Feed";
 import Connections from "./components/Connections";
 import Requests from "./components/Request";
 import Premium from "./components/Premium";
+import Chat from "./components/Chat";
 
 function App() {
   return (
@@ -22,7 +23,7 @@ function App() {
               <Route path="/connections" element={<Connections />} />
               <Route path="/requests" element={<Requests />} />
               <Route path="/premium" element={<Premium />} />
-              
+              <Route path="/chat/:toUserId" element={<Chat />} />
             </Route>
           </Routes>
         </BrowserRouter>
