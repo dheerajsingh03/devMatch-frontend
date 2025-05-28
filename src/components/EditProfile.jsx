2f21ae212320ee866482a4fc8a51eea1b6081419
Dheeraj Singh <your.email@dheerajsingh2100@gmail.com>
2025-05-28 21:26:06 +0530

    bug fix

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -1,30 +1,24 @@
+import { useState } from "react";
+import UserCard from "./UserCard";
 import axios from "axios";
-import React, { useState } from "react";
 import { BASE_URL } from "../utils/constants";
 import { useDispatch } from "react-redux";
-import UserCard from "./UserCard";
 import { addUser } from "../utils/userSlice";
 
 const EditProfile = ({ user }) => {
   const [firstName, setFirstName] = useState(user.firstName);
-
   const [lastName, setLastName] = useState(user.lastName);
-
-  const [photoUrl, setPhotoUrl] = useState(user.photoUrl||"");
-
+  const [photoUrl, setPhotoUrl] = useState(user.photoUrl);
   const [age, setAge] = useState(user.age || "");
-
-  const [gender, setGender] = useState(user.gender||"");
-
-  const [about, setAbout] = useState(user.about||"");
-
-  const [errors, setError] = useState("");
-
+  const [gender, setGender] = useState(user.gender || "");
+  const [about, setAbout] = useState(user.about || "");
+  const [error, setError] = useState("");
   const dispatch = useDispatch();
-
   const [showToast, setShowToast] = useState(false);
 
-  const handleSaveProfile = async () => {
+  const saveProfile = async () => {
+    //Clear Errors
+    setError("");
     try {
       const res = await axios.patch(
         BASE_URL + "/profile/edit",
@@ -38,104 +32,94 @@ const EditProfile = ({ user }) => {
         },
         { withCredentials: true }
       );
-      dispatch(addUser(res.data.data));
+      dispatch(addUser(res?.data?.data));
       setShowToast(true);
       setTimeout(() => {
         setShowToast(false);
       }, 3000);
     } catch (err) {
-      setError(err);
-      console.log(err);
+      setError(err.response.data);
     }
   };
+
   return (
     <>
-      <div className="flex justify-center">
-        <div className="flex justify-center my-10 ">
+      <div className="flex justify-center my-10">
+        <div className="flex justify-center mx-10">
           <div className="card bg-base-300 w-96 shadow-xl">
             <div className="card-body">
               <h2 className="card-title justify-center">Edit Profile</h2>
               <div>
                 <label className="form-control w-full max-w-xs my-2">
                   <div className="label">
-                    <span className="label-text">First Name</span>
+                    <span className="label-text">First Name:</span>
                   </div>
                   <input
+                    type="text"
                     value={firstName}
-                    onChange={(e) => {
-                      setFirstName(e.target.value);
-                    }}
-                    type="text"
                     className="input input-bordered w-full max-w-xs"
+                    onChange={(e) => setFirstName(e.target.value)}
                   />
                 </label>
                 <label className="form-control w-full max-w-xs my-2">
+                  <label className="form-control w-full max-w-xs my-2">
+                    <div className="label">
+                      <span className="label-text">Last Name:</span>
+                    </div>
+                    <input
+                      type="text"
+                      value={lastName}
+                      className="input input-bordered w-full max-w-xs"
+                      onChange={(e) => setLastName(e.target.value)}
+                    />
+                  </label>
                   <div className="label">
-                    <span className="label-text">Last Name</span>
+                    <span className="label-text">Photo URL :</span>
                   </div>
                   <input
-                    value={lastName}
-                    onChange={(e) => {
-                      setLastName(e.target.value);
-                    }}
                     type="text"
-                    className="input input-bordered w-full max-w-xs"
-                  />
-                </label>
-                <label className="form-control w-full max-w-xs my-2">
-                  <div className="label">
-                    <span className="label-text">Photo Url</span>
-                  </div>
-                  <input
                     value={photoUrl}
-                    onChange={(e) => {
-                      setPhotoUrl(e.target.value);
-                    }}
-                    type="text"
                     className="input input-bordered w-full max-w-xs"
+                    onChange={(e) => setPhotoUrl(e.target.value)}
                   />
                 </label>
                 <label className="form-control w-full max-w-xs my-2">
                   <div className="label">
-                    <span className="label-text">Age</span>
+                    <span className="label-text">Age:</span>
                   </div>
                   <input
+                    type="text"
                     value={age}
-                    onChange={(e) => {
-                      setAge(e.target.value);
-                    }}
-                    type="text"
                     className="input input-bordered w-full max-w-xs"
+                    onChange={(e) => setAge(e.target.value)}
                   />
                 </label>
                 <label className="form-control w-full max-w-xs my-2">
                   <div className="label">
-                    <span className="label-text">Gender</span>
+                    <span className="label-text">Gender:</span>
                   </div>
                   <input
-                    value={gender}
-                    onChange={(e) => {
-                      setGender(e.target.value);
-                    }}
                     type="text"
+                    value={gender}
                     className="input input-bordered w-full max-w-xs"
+                    onChange={(e) => setGender(e.target.value)}
                   />
                 </label>
                 <label className="form-control w-full max-w-xs my-2">
                   <div className="label">
-                    <span className="label-text">About</span>
+                    <span className="label-text">About:</span>
                   </div>
-                  <textarea
-                    className="textarea textarea-bordered"
-                    placeholder=""
+                  <input
+                    type="text"
                     value={about}
+                    className="input input-bordered w-full max-w-xs"
                     onChange={(e) => setAbout(e.target.value)}
-                  ></textarea>
+                  />
                 </label>
-                <p className="text-red-600">{errors}</p>
               </div>
+              <p className="text-red-500">{error}</p>
               <div className="card-actions justify-center m-2">
-                <button className="btn btn-primary" onClick={handleSaveProfile}>
+                <button className="btn btn-primary" onClick={saveProfile}>
                   Save Profile
                 </button>
               </div>
@@ -149,12 +133,11 @@ const EditProfile = ({ user }) => {
       {showToast && (
         <div className="toast toast-top toast-center">
           <div className="alert alert-success">
-            <span>Profile saved Successfuly</span>
+            <span>Profile saved successfully.</span>
           </div>
         </div>
       )}
     </>
   );
 };
-
 export default EditProfile;
